76298f8e3d7788d3df1551f4ea9ba5de0cbf378b
vladimir.milosevic <umarkovic864@live.com>
2024-02-07 20:26:48 +0100

    subjects

--- apps/past-exams-hub-ui/src/app/subjects/subjects.component.ts
+++ apps/past-exams-hub-ui/src/app/subjects/subjects.component.ts
@@ -1,12 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'pastexamshub-subjects',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatTableModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './subjects.component.html',
   styleUrl: './subjects.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SubjectsComponent {}
+export class SubjectsComponent {
+  displayedColumns: string[] = [
+    'nameSubject',
+    'type',
+    'professors',
+    'studies',
+    'year',
+    'espb',
+  ];
+  dataSource = new MatTableDataSource([]);
+}
